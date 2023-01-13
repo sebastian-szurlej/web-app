bfd7c0d644e60859f9df42dfdd2fdc7a89e79a23
sebek1911 <sebastian.szurlej@gmail.com>
2023-01-13 20:24:36 +0100

    Init setup

---
+++ src-ui/layouts/AppHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app-bar>
+    <v-app-bar-title>Title</v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ src-ui/pages/PageTwo/PageTwo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-h4">This is a Page Two</div>
+</template>
+
+<style></style>
