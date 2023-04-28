d3a7556a75b0fa992b397be074d56efe7856a5c2
sebek1911 <sebastian.szurlej@gmail.com>
2023-04-28 08:26:15 +0200

    implement simple checkbox

--- src-ui/pages/PageTwo/PageTwo.vue
+++ src-ui/pages/PageTwo/PageTwo.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="text-h4">This is a Page Two</div>
+  <div class="custom-div d-flex flex-column">
+    <v-checkbox
+      v-model="selected"
+      v-for="item in aircrafts"
+      :key="item.id"
+      :label="item.tailNumber"
+      :value="item.id"
+    ></v-checkbox>
+  </div>
 </template>
 
-<style></style>
+<script setup>
+import { ref, watch } from "vue";
+const selected = ref([]);
+console.log(selected.value);
+
+const aircrafts = [
+  { id: 1, tailNumber: "110" },
+  { id: 2, tailNumber: "120" },
+  { id: 3, tailNumber: "130" },
+  { id: 4, tailNumber: "140" },
+  { id: 6, tailNumber: "150" },
+  { id: 7, tailNumber: "160" },
+  { id: 8, tailNumber: "170" },
+  { id: 9, tailNumber: "180" },
+  { id: 10, tailNumber: "190" },
+];
+watch(selected, () => {
+  console.log(selected);
+});
+</script>
+
+<style scoped>
+.custom-div {
+  max-height: 300px;
+  height: 300px;
+}
+</style>
